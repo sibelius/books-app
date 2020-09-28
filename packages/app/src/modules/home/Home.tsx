5fd79ce655481534f8941cd93070858f576496af
Jean Leonço <jeang.leonco@gmail.com>
2020-09-28 19:13:20 -0300

    feat(setup-relay): changes relay experimental version

--- packages/app/src/modules/home/Home.tsx
+++ packages/app/src/modules/home/Home.tsx
@@ -2,8 +2,10 @@ import React from 'react';
 import { View, Text } from 'react-native';
 import { graphql, useLazyLoadQuery } from 'react-relay/hooks';
 
+import { HomeQuery } from './__generated__/HomeQuery.graphql';
+
 const Home = () => {
-  const data = useLazyLoadQuery(
+  const data = useLazyLoadQuery<HomeQuery>(
     graphql`
       query HomeQuery {
         me {
