5c20f0b13ffc397cd319bf435175c2b880cfcb5f
Jean Leonço <jeang.leonco@gmail.com>
2020-09-29 22:54:20 -0300

    feat(ui-package): add ui package

---
+++ packages/ui/src/common/theme.tsx
@@ -0,0 +1,58 @@
+const theme = {
+  colors: {
+    primary: '#FF9900',
+    gradient: '#FF590D',
+    black: '#222222',
+    background: '#F5F5F5',
+    white: '#ffffff',
+    text: '#666666',
+    confirm: '#0E547F',
+    warning: '#B30302',
+    link: '#276EF1',
+  },
+  fontSizes: {
+    h1: '50px',
+    h3: '20px',
+    button: '18px',
+    label: '16px',
+    text: '14px',
+  },
+  fontWeights: {
+    regular: '400',
+    semiBold: '600',
+    bold: '700',
+    extraBold: '800',
+  },
+};
+
+// @TODO - improve type
+declare module 'styled-components' {
+  export interface DefaultTheme {
+    colors: {
+      primary: string;
+      gradient: string;
+      black: string;
+      background: string;
+      white: string;
+      text: string;
+      confirm: string;
+      warning: string;
+      link: string;
+    };
+    fontSizes: {
+      h1: string;
+      h2: string;
+      button: string;
+      text: string;
+      label: string;
+    };
+    fontWeights: {
+      regular: string;
+      semiBold: string;
+      bold: string;
+      extraBold: string;
+    };
+  }
+}
+
+export default theme;
---
+++ packages/ui/src/index.ts
@@ -0,0 +1,13 @@
+// Common
+export { default as theme } from './common/theme';
+export { default as Button } from './common/Button';
+export { default as Column } from './common/Column';
+export { default as ErrorBoundary } from './common/ErrorBoundary';
+export { default as Row } from './common/Row';
+export { default as Space } from './common/Space';
+export { default as Text } from './common/Text';
+export { default as TextInput } from './common/TextInput';
+
+// Form
+export { default as FormikButton } from './form/FormikButton';
+export { default as FormikInput } from './form/FormikInput';
